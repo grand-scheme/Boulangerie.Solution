5447c2583a7f9cd8d3e61d3cfcc3249407845c40
Shannon Grantski <grantski@pm.me>
2020-12-11 13:26:53 -0800

    add initial code to test for cost

--- Boulangerie/Models/Bread.cs
+++ Boulangerie/Models/Bread.cs
@@ -14,5 +14,11 @@ namespace Boulangerie
       breadWithDiscount = (2 * breadWithDiscount / 3);
       return breadWithDiscount;
     }
+    public int BreadFinalCost(int breadQuantity)
+    {
+      // in this test, bread input quantity is 30
+      int breadToCalc = BreadFullPrice(breadQuantity); // should return 2/3s bread input, so 20
+      return breadToCalc; // at this point, should still return 20
+    }
   }
 }
