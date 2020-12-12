cdacd948d7d88def7cb7b81d5d114d9a5b5eae50
Shannon Grantski <grantski@pm.me>
2020-12-11 16:26:01 -0800

    move pastry shopping to own console, add tryparse

--- Boulangerie/Program.cs
+++ Boulangerie/Program.cs
@@ -1,19 +1,43 @@
 using System;
+
 namespace Boulangerie
 {
 class ConsoleInterface
 {
+static void PastryConsole()
+{
+  Console.WriteLine("how many u want");
+  string input = Console.ReadLine();
+  int pastry = int.TryParse(input, out pastry) ? pastry:0;
+  Console.WriteLine("So you want " + pastry + " pastries?");
+  Pastry cartTotal = new Pastry();
+  int pastryCost = cartTotal.PastryFinalCost(pastry);
+  Console.WriteLine("That'll cost you $" + pastryCost +". You good with that?");
+}
+// static void BreadConsole()
+// {
+// }
+
+
+
+
+
+
 static void Main()
 {
 Console.WriteLine("Hello");
 Console.WriteLine("Select: bread or pastry");
 string selection = Console.ReadLine().ToLower();
 Console.WriteLine("You chose " + selection);
-
-
-
-
-
+if (selection == "pastry") 
+{
+PastryConsole();
+}
+else
+{
+  Console.WriteLine("i'll get to u later");
+  Main();
+}
 
 
 
