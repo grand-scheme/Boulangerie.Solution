306d20454030de58ef6263f6e1eaea9729d6f79d
Shannon Grantski <grantski@pm.me>
2020-12-11 16:36:24 -0800

    add method for confirmations

--- Boulangerie/Program.cs
+++ Boulangerie/Program.cs
@@ -4,15 +4,33 @@ namespace Boulangerie
 {
 class ConsoleInterface
 {
+static void YesOrNoQuestion(string confirmation)
+{
+  if (confirmation == "YES")
+{
+}
+else if (confirmation == "NO")
+{
+}
+else
+{
+  Console.WriteLine("Wanna run that by me one more time? YES or NO?");
+  string repeat = Console.ReadLine().ToUpper();
+  YesOrNoQuestion(repeat);
+}
+}
+
 static void PastryConsole()
 {
   Console.WriteLine("how many u want");
-  string input = Console.ReadLine();
-  int pastry = int.TryParse(input, out pastry) ? pastry:0;
+  int pastry = int.TryParse(Console.ReadLine(), out pastry) ? pastry : 0;
   Console.WriteLine("So you want " + pastry + " pastries?");
   Pastry cartTotal = new Pastry();
   int pastryCost = cartTotal.PastryFinalCost(pastry);
   Console.WriteLine("That'll cost you $" + pastryCost +". You good with that?");
+  Console.WriteLine("You can answer YES or NO");
+  string confirmation = Console.ReadLine().ToUpper();
+  YesOrNoQuestion(confirmation);
 }
 // static void BreadConsole()
 // {
