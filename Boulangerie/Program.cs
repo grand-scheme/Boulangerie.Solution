5538f25aeeb3c60db12bed1f50ac478b5e2dfecd
Shannon Grantski <grantski@pm.me>
2020-12-13 21:06:56 -0800

    add spacing and opener/closer

--- Boulangerie/Program.cs
+++ Boulangerie/Program.cs
@@ -9,44 +9,83 @@ namespace Boulangerie
       ConsoleBackground question = new ConsoleBackground();
       return question.YesOrNoQuestion();
     }
-    static bool Checkout(int x, int y)
+    static bool Checkout(int breadCart, int pastryCart)
     {
       ConsoleBackground checkout = new ConsoleBackground();
-      return checkout.CheckoutCart(x, y);
+      return checkout.EndOfTransaction(breadCart, pastryCart);
     }
     static int BuyBread()
     {
       ConsoleCalcs buyBread = new ConsoleCalcs();
-      return buyBread.BreadConsole();
+      return buyBread.Purchases("BREAD");
     }
     static int BuyPastries()
     {
       ConsoleCalcs buyPastries = new ConsoleCalcs();
-      return buyPastries.PastryConsole();
+      return buyPastries.Purchases("PASTRY");
     }
+    static void Closer()
+    {
+      Console.WriteLine("Great! We'll have those ready for you at 4 AM.");
+      Console.WriteLine(" ");
+      Console.WriteLine("╔═══════════════════════════════════════════╗");
+      Console.WriteLine("║                                           ║");
+      Console.WriteLine("║           THANK YOU FOR CHOOSING          ║");
+      Console.WriteLine("║                                           ║");
+      Console.WriteLine("║            PIERRE'S BOULANGERIE           ║");
+      Console.WriteLine("║                                           ║");
+      Console.WriteLine("╠═══════════════════════════════════════════╣");
+      Console.WriteLine("║                                           ║");
+      Console.WriteLine("║           ! NOW OPEN WEEKDAYS !           ║");
+      Console.WriteLine("║           ! NOW OPEN WEEKENDS !           ║");
+      Console.WriteLine("║                                           ║");
+      Console.WriteLine("╚═══════════════════════════════════════════╝");
+    }
+
+// UI CONSOLE BEGINS HERE
     static void Main()
     {
       int breadCart = 0;
       int pastryCart = 0;
-
-      Console.WriteLine("Hello");
-      Console.WriteLine("Select: bread or pastry");
-      string selection = Console.ReadLine().ToUpper();
+      Console.WriteLine("╔═══════════════════════════════════════════╗");
+      Console.WriteLine("║            PIERRE'S BOULANGERIE           ║");
+      Console.WriteLine("╠═══════════════════════════════════════════╣");
+      Console.WriteLine("║   - BREAD: $5 PER LOAF                    ║");
+      Console.WriteLine("║   SPECIAL: BUY TWO LOAVES, GET ONE FREE   ║");
+      Console.WriteLine("╟───────────────────────────────────────────╢");
+      Console.WriteLine("║   - PASTRIES: $2 PER PASTRY               ║");
+      Console.WriteLine("║   SPECIAL: 3 PASTRIES FOR $5              ║");
+      Console.WriteLine("╚═══════════════════════════════════════════╝");
+      Console.WriteLine(" ");
+      Console.WriteLine("Welcome! Please select what you would like to purchase today.");
+      Console.WriteLine("Type: BREAD or PASTRY");
       
+      string selection = Console.ReadLine().ToUpper();
       if (selection == "PASTRY" || selection == "P") 
       {
         pastryCart = BuyPastries();
 
-        Console.WriteLine("Your pastry total is $" + pastryCart);
-        Console.WriteLine("did you want some bread too?");
+        Console.WriteLine(" ");
+        Console.WriteLine("Would you like some bread as well?");
         if (YesOrNo() == true) { breadCart = BuyBread(); }
+      
+        Console.WriteLine(" ");
         Console.WriteLine("Are you ready to check out?");
         if (YesOrNo() == true)
         {
-          if (Checkout(breadCart, pastryCart) == false) { Main(); }
+          if (Checkout(breadCart, pastryCart) == false) 
+          { 
+            Main(); 
+          }
+          else
+          {
+            Closer();
+          }
         }
         else
         {
+          Console.WriteLine("That's okay. Let's take it from the top.");
+          Console.WriteLine(" ");
           Main();
         }
       }
@@ -54,19 +93,29 @@ namespace Boulangerie
       {
         breadCart = BuyBread();
 
-        Console.WriteLine("Your bread total is $" + breadCart);
-        Console.WriteLine("did you want some pastries too?");
+        Console.WriteLine(" ");
+        Console.WriteLine("Would you like some pastries too?");
         if (YesOrNo() == true)
         {
           pastryCart = BuyPastries();
         }
+        Console.WriteLine(" ");
         Console.WriteLine("Are you ready to check out?");
         if (YesOrNo() == true)
         {
-          if (Checkout(breadCart, pastryCart) == false) { Main(); }
+          if (Checkout(breadCart, pastryCart) == false) 
+          { 
+            Main(); 
+          }
+          else
+          {
+            Closer();
+          }
         }
         else
         {
+          Console.WriteLine("That's okay. Let's take it from the top.");
+          Console.WriteLine(" ");
           Main();
         }
       }
