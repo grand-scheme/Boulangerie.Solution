a1449534edf4a82b2d120353bcd8b71fc1beb1af
Shannon Grantski <grantski@pm.me>
2020-12-13 16:02:48 -0800

    test for price toggle

--- Boulangerie.Tests/ModelTests/BuyTwoGetOneXTests.cs
+++ Boulangerie.Tests/ModelTests/BuyTwoGetOneXTests.cs
@@ -11,6 +11,11 @@ namespace Boulangerie.Tests
       BuyOneGetTwoX testProductInput = new BuyOneGetTwoX();
       Assert.AreEqual("bread", testProductInput.NoticeProduct("bread"));
     }
-
+    [TestMethod]
+    public void SetPrice_CanSetPricesBasedOnInput_True()
+    {
+      BuyOneGetTwoX testPrices = new BuyOneGetTwoX();
+      Assert.AreEqual(5, testPrices.SetPrice("bread"));
+    }
   }
 }
--- Boulangerie/Models/BuyTwoGetOneX.cs
+++ Boulangerie/Models/BuyTwoGetOneX.cs
@@ -4,7 +4,34 @@ namespace Boulangerie
   {
     public string NoticeProduct (string input)
     {
-      return "pastry";
+      if (input == "bread" || input == "b") 
+      {
+        return "bread";
+      } 
+      else if (input == "pastry" || input == "p") 
+      {
+      return "pastry"; 
+      }
+      else 
+      {
+        return "bad input";
+      }
+    }
+    public int SetPrice (string input)
+    {
+      int price = 100;
+      int discount = 100;
+      if (NoticeProduct(input) == "bread")
+      { 
+        price = 5;
+        discount = 0;
+      }
+      else
+      {
+        price = 2;
+        discount = 1;
+      }
+      return price;
     }
   }
 }
