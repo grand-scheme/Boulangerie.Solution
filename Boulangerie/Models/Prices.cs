5cfbbc5ab9bde2ccd940b30733452ab7d8021021
Shannon Grantski <grantski@pm.me>
2020-12-20 17:42:06 -0800

    switch security to Protected

--- Boulangerie/Models/Prices.cs
+++ Boulangerie/Models/Prices.cs
@@ -2,19 +2,19 @@ namespace Boulangerie.Models
 {
   public abstract class Prices
   {
-    public int PastryFullPrice()
+    protected int PastryFullPrice()
     {
       return 2;
     }
-    public int PastryDiscountPrice()
+    protected int PastryDiscountPrice()
     {
       return 1;
     }
-    public int BreadFullPrice()
+    protected int BreadFullPrice()
     {
       return 5;
     }
-    public int BreadDiscountPrice()
+    protected int BreadDiscountPrice()
     {
       return 0;
     }
