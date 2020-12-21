2784f4e786004380d16b0116bdd4a2f58d3fb411
Shannon Grantski <grantski@pm.me>
2020-12-20 23:45:01 -0800

    add overload and verbage to Bread.cs

--- Boulangerie/Models/Bread.cs
+++ Boulangerie/Models/Bread.cs
@@ -2,17 +2,28 @@ namespace Boulangerie.Models
 {
   public class Bread2
   {
-    public int BreadQuantity { get; set; } 
+    public int Quantity { get; set; }
+    public string Product { get; }
+    public string Plural { get; }
+    public string Singular { get; }
+    public string DealMinusOne { get; }
     
-    public Bread2(int breadQuantity)
+    public Bread2()
     {
-      BreadQuantity = breadQuantity;
+      Product = "bread";
+      Plural = "loaves of bread";
+      Singular = "loaf of bread";
+      DealMinusOne = "It's 'Buy two, get one free' for our bread. If you add one more, it's on the house. Why would you say no to free bread?";
     }
-
+    public Bread2(int quantity)
+    : this()
+    {
+      Quantity = quantity;
+    }
     public int BreadTotalCost()
     {
       Buy2Get1 dummy = new Buy2Get1();
-      return dummy.FinalCost(BreadQuantity, "BREAD");
+      return dummy.FinalCost(Quantity, "BREAD");
     }
   }
 }
