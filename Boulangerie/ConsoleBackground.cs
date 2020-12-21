b9023a7e2c69ed6fbe3921b8c75f21ae2792f6d6
Shannon Grantski <grantski@pm.me>
2020-12-21 04:10:31 -0800

    adjust spacing

--- Boulangerie/ConsoleBackground.cs
+++ Boulangerie/ConsoleBackground.cs
@@ -1,33 +1,32 @@
 using System;
-using Boulangerie.Models;
 
 namespace Boulangerie
 {
   public abstract class ConsoleBackground
+  {
+    public static void LineBreak()
     {
-      public static void LineBreak()
-      {
-        Console.WriteLine(" ");
+      Console.WriteLine(" ");
+    }
+    public static bool YesOrNo()
+    {
+      Console.WriteLine("You can answer YES or NO");
+      string confirmation = Console.ReadLine().ToUpper();
+      if (confirmation == "YES" || confirmation == "Y")
+      { 
+        return true;
       }
-      public static bool YesOrNo()
-      {
-        Console.WriteLine("You can answer YES or NO");
-        string confirmation = Console.ReadLine().ToUpper();
-        if (confirmation == "YES" || confirmation == "Y")
-        { 
-          return true;
-        }
-        else if (confirmation == "NO" || confirmation == "N")
-        { 
-          return false; 
-        }
-        else
-        { 
-          Console.WriteLine("Wanna run that by me one more time?");
-          LineBreak();
-          LineBreak();
-          return YesOrNo();
-        }
+      else if (confirmation == "NO" || confirmation == "N")
+      { 
+        return false; 
+      }
+      else
+      { 
+        Console.WriteLine("Wanna run that by me one more time?");
+        LineBreak();
+        LineBreak();
+        return YesOrNo();
       }
     }
+  }
 }
