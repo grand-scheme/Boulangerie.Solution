c726ac539593faf5ce805c72f5bd4259c22e13b3
Shannon Grantski <grantski@pm.me>
2020-12-20 18:56:34 -0800

    add test for expected price (quantity 1)

--- Boulangerie.Tests/ModelTests/Bread2Tests.cs
+++ Boulangerie.Tests/ModelTests/Bread2Tests.cs
@@ -30,5 +30,15 @@ namespace Boulangerie.Models.Tests
       int test3Quantity = test3.GetBreadQuantity();
       Assert.AreEqual(typeof(int), test3Quantity.GetType());
     }
+    // end of test 15
+    [TestMethod] // test 16
+    public void Bread2_BreadTotalCostReturnsCorrectPrice_Int()
+    {
+      Bread2 test4 = new Bread2();
+      test4.SetQuantity(1);
+      int test4Price = test4.BreadTotalCost();
+      int expectedPrice = 10;
+      Assert.AreEqual(expectedPrice, test4Price);
+    }
   }
 }
