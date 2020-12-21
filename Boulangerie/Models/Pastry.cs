05873001f6a80599c1efe46ff216da1e15bbd6e5
Shannon Grantski <grantski@pm.me>
2020-12-21 00:00:11 -0800

    add overload constructor and verbage

--- Boulangerie/Models/Pastry.cs
+++ Boulangerie/Models/Pastry.cs
@@ -2,17 +2,28 @@ namespace Boulangerie.Models
 {
   public class Pastry2
   {
-    public int PastryQuantity { get; set; } 
+    public int Quantity { get; set; } 
+    public string Product { get; }
+    public string Plural { get; }
+    public string Singular { get; }
+    public string DealMinusOne { get; }
     
-    public Pastry2(int pastryQuantity)
+    public Pastry2()
     {
-      PastryQuantity = pastryQuantity;
+      Product = "pastry";
+      Plural = "pastries";
+      Singular = "pastries";
+      DealMinusOne = "It's 'Buy three for $5` for our pastries. It's only a dollar to add one more to your bag. Do you want to add one more?";
+    }
+    public Pastry2(int quantity)
+    {
+      Quantity = quantity;
     }
 
     public int PastryTotalCost()
     {
       Buy2Get1 dummy = new Buy2Get1();
-      return dummy.FinalCost(PastryQuantity, "PASTRY");
+      return dummy.FinalCost(Quantity, "PASTRY");
     }
   }
 }
