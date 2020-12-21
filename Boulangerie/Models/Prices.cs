eb736b696a27eb0ecb46baf7de6a606b7bfeac98
Shannon Grantski <grantski@pm.me>
2020-12-20 19:40:45 -0800

    consolidate discount (buy2get1) into Prices.cs

--- Boulangerie/Models/Prices.cs
+++ Boulangerie/Models/Prices.cs
@@ -19,4 +19,41 @@ namespace Boulangerie.Models
       return 0;
     }
   }
+
+  public class Buy2Get1 : Prices
+  {
+    int SoldAtDiscount(int quantity)
+    {
+      int outsideDiscount = (quantity % 3);
+      int insideDiscount = quantity - outsideDiscount;
+      return (insideDiscount / 3);
+    }
+
+    int SoldAtFullPrice(int quantity)
+    {
+      return quantity - SoldAtDiscount(quantity);
+    }
+
+    public int FinalCost(int quantity, string type)
+    {
+      if (type == "bread")
+      {
+        return
+        (
+          (SoldAtFullPrice(quantity) * BreadFullPrice())
+          +
+          (SoldAtDiscount(quantity) * BreadDiscountPrice())
+        );
+      }
+      else // if (type == "pastry") 
+      {
+        return 
+        (
+          (SoldAtFullPrice(quantity) * PastryFullPrice()) 
+          +
+          (SoldAtDiscount(quantity) * PastryDiscountPrice())
+        );
+      }
+    }
+  }
 }
