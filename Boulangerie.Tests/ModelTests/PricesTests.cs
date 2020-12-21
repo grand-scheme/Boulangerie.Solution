2f9f4538ef5891692d378abe08b03af8c0cc941d
Shannon Grantski <grantski@pm.me>
2020-12-20 19:56:41 -0800

    add test to verify discount quantity

--- Boulangerie.Tests/ModelTests/PricesTests.cs
+++ Boulangerie.Tests/ModelTests/PricesTests.cs
@@ -6,12 +6,21 @@ namespace Boulangerie.Models.Tests
   public class PricesTests
   {
     [TestMethod] // test 19
-    public void GetSoldAtDiscount_ReturnsInt_Int()
+    public void GetSoldAtDiscount_ReturnsInt_SystemInt()
     {
       Buy2Get1 dummy = new Buy2Get1();
       int test19 = dummy.GetSoldAtDiscount(3);
       Assert.AreEqual(typeof(int), test19.GetType());
     }
     // end of test 19
+    [TestMethod] // test 20
+    public void GetSoldAtDiscount_ReturnsQuantityOfProductSoldAtDiscountPrice_Int()
+    {
+      Buy2Get1 dummy = new Buy2Get1();
+      int test20 = dummy.GetSoldAtDiscount(3);
+      int expectedQuantity = 3;
+      Assert.AreEqual(expectedQuantity, test20);
+    }
+    // end of test 20
   }
 }
