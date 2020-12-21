e3a4d38092a017d6468596409e47a24e47f89bcd
Shannon Grantski <grantski@pm.me>
2020-12-21 02:40:21 -0800

    use objects as intended, create abstract class

--- Boulangerie/ConsoleBackground.cs
+++ Boulangerie/ConsoleBackground.cs
@@ -1,65 +1,80 @@
 using System;
+using Boulangerie.Models;
 
 namespace Boulangerie
 {
-  public class ConsoleBackground
-  {
-    public bool YesOrNoQuestion()
+  public abstract class FrequentCallback
     {
-      Console.WriteLine("You can answer YES or NO");
-      string confirmation = Console.ReadLine().ToUpper();
-      if (confirmation == "YES" || confirmation == "Y")
-      { 
-        return true;
-      }
-      else if (confirmation == "NO" || confirmation == "N")
-      { 
-        return false; 
-      }
-      else
-      { 
-        Console.WriteLine("Wanna run that by me one more time?");
+      public static void LineBreak()
+      {
         Console.WriteLine(" ");
-        Console.WriteLine(" ");
-        return YesOrNoQuestion();
       }
+      public static bool YesOrNo()
+      {
+        Console.WriteLine("You can answer YES or NO");
+        string confirmation = Console.ReadLine().ToUpper();
+        if (confirmation == "YES" || confirmation == "Y")
+        { 
+          return true;
+        }
+        else if (confirmation == "NO" || confirmation == "N")
+        { 
+          return false; 
+        }
+        else
+        { 
+          Console.WriteLine("Wanna run that by me one more time?");
+          LineBreak();
+          LineBreak();
+          return YesOrNo();
+        }
+      }
+    
     }
-    public bool EndOfTransaction(int breadCart, int pastryCart)
+  public class ConsoleBackground : FrequentCallback
+  {
+    public bool EndOfTransaction(object bread, object pastry)
     {
-    if ((breadCart  == 0) && (pastryCart == 0)) 
+      Bread2 testBread = (Bread2)bread;
+      int breadQuantity = testBread.Quantity;
+      int breadPrice = testBread.BreadTotalCost();
+      
+      Pastry2 testPastry = (Pastry2)pastry;
+      int pastryQuantity = testPastry.Quantity;
+      int pastryPrice = testPastry.PastryTotalCost();
+      
+      if ((breadQuantity == 0) && (pastryQuantity == 0)) 
       { 
         Console.WriteLine("Oh, you're funny.");
         Console.WriteLine("Just for that, we have to do this all again.");
         Console.WriteLine("I hope you're happy.");
-        Console.WriteLine(" ");
-        Console.WriteLine(" ");
-        Console.WriteLine(" ");
-        Console.WriteLine(" ");
+        LineBreak();
+        LineBreak();
+        LineBreak();
+        LineBreak();
         return false;
       }
-      else if ((breadCart  != 0) && (pastryCart != 0)) 
+      else if ((breadQuantity  != 0) && (pastryQuantity != 0)) 
       { 
-        Console.WriteLine("Your grand total is $" + (breadCart + pastryCart)); 
-        Console.WriteLine(" ");
+        Console.WriteLine("For " + testBread.Product + ", it looks like you're getting " + breadQuantity + " at " + breadPrice + ".");
+        Console.WriteLine("And for " + testPastry.Plural + ", it's " + pastryQuantity + " at " + pastryPrice + ".");
+        Console.WriteLine("Your grand total is $" + (breadPrice + pastryPrice)); 
+        LineBreak();
         return true;
       }
-      else if (breadCart  != 0) 
+      else if (breadQuantity  != 0) 
       { 
-        Console.WriteLine("Your total is $" + breadCart); 
-        Console.WriteLine(" ");
+        Console.WriteLine("For " + testBread.Product + ", it looks like you're getting " + breadQuantity);
+        Console.WriteLine("Your total is $" + breadPrice); 
+        LineBreak();
         return true;
       }
-      else if (pastryCart != 0) 
+      else // if (pastryQuantity != 0) 
       { 
-        Console.WriteLine("Your total is $" + pastryCart); 
-        Console.WriteLine(" ");
+        Console.WriteLine("For " + testPastry.Plural + ", it looks like you're getting " + pastryQuantity);
+        Console.WriteLine("Your total is $" + pastryPrice); 
+        LineBreak();
         return true;
-      }
-      else 
-      { 
-        Console.WriteLine("¯\\_(ツ)_/¯");
-        Console.WriteLine(" ");
-        return false;
       }
     }
   }
